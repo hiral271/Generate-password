7e06c097a69f9f3e71a361086dbcbea358f17905
Hiral patel <hiralm227@gmail.com>
2021-02-10 19:17:01 -0500

    issue done with button and added comments

--- assest/index.js
+++ assest/index.js
@@ -1,16 +1,16 @@
 
-
+//defining arrays 
 const usableCharactor =[]
 const capitalCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 const specialCase =['@','#','$','%','&','*','+']
 const numeric = ['0','1','2','3','4','5','6','7','8','9']
-const generatePassword = []
+const Password = []
 
 
 
 
-
+//user choosen  password cases to loop 
 const collectWord = function(arr){
     for(let i = 0;i<arr.length; i++){
 usableCharactor.push(arr[i]);
@@ -18,9 +18,9 @@ usableCharactor.push(arr[i]);
     }
 }
 
-const generateBtn = document.getElementById('generate');
 
 
+//prompt will pop up on screen to choose charactors 
 function getPasswordLength(){
 
 let passwordlength = prompt("select password lenth between min 8 to max 128");
@@ -32,45 +32,55 @@ console.log(passwordlength)
 return passwordlength;
 }
 
-
+// function for make user choice from diffrent cases
 function alphabets(){
 
 const  passwordLength = getPasswordLength();
 const lower =confirm("do you wanna select Lower case ?")
 
+//let user to  choose lowercase
 if (lower){
     collectWord(lowerCase);
 };
 
+//let user to  choose capitalcase
 const capital = confirm("do you wanna select Capital case ?")
 if (capital){
 
     collectWord(capitalCase)
 };
+
+//let user to  choose numeric
 const number = confirm("do you wanna select number ?")
 if (number){
     collectWord(numeric)
 };
+
+//let user to  choose specialcase
 const special = confirm("do you wanna select special case ?")
 if (special){
     collectWord(specialCase)
 };
+
+
+//password is generate base on user's choice
 console.log(usableCharactor)
 password= [];
 
+
+//create loop to choose passowrd charactors
 for(let i=0; i<passwordLength; i++){
     
 var index = Math.floor(Math.random() * usableCharactor.length);
 password.push(usableCharactor[index]);
 };
 
-
+//write passowrd to screen 
 password  = password.join("");
 
 document.getElementById('password').value = password;
 
 
-generateBtn.addEventListener('click', generate);
 
 }
 
@@ -81,8 +91,6 @@ generateBtn.addEventListener('click', generate);
 
 
 
-
-alphabets();
 
 
 
